8da679fade27fe03ff6fa6557fce39444e673034
manojkmishra <manojmishra571980@gmail.com>
2020-09-21 17:28:42 +1000

    socialite install, configure

---
+++ app/Http/Controllers/Auth/socialacctController'.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\socialacct;
+
+class socialacctController extends Controller
+{
+    public function redirecToProvider($provider){
+    }
+    public function handleProviderCallback(){
+    }
+}
--- database/migrations/2020_09_21_054746_create_socialaccts_table.php
+++ database/migrations/2020_09_21_054746_create_socialaccts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSocialacctsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('socialaccts', function (Blueprint $table) {
             $table->bigIncrements('id');
-           // $table->increments('id');
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->bigInteger('user_id');
+            $table->string('provider_name');
+            $table->string('provider_id')->unique()->nullable();
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('socialaccts');
     }
 }
